b546eaefe4076f775586471b05f2a5a716f8bdd8
Alexander Najafi <alex@esek.se>
2015-03-24 13:50:24 +0100

    Updated .gitignore file. Wrote config mask for spi setup on the dsp when using the dsp as a slave.

--- dsp/VDSPProject/VDSPProject.c
+++ dsp/VDSPProject/VDSPProject.c
@@ -1,9 +1,15 @@
-/*****************************************************************************
- * VDSPProject.c
- *****************************************************************************/
+#include <processor_include.h>
+#include <sysreg.h>
+#include <signal.h>
+#include <string.h>
+#include <filter.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main( void )
-{
-	/* Begin adding your custom code here */
-	return 0;
+#include "dsp_setup.h"
+#include "framework.h"
+
+void main()
+{   
+	spi_init();
 }
---
+++ dsp/VDSPProject/dsp_setup.h
@@ -0,0 +1,2 @@
+void spi_init(void);
+void memreg_write(unsigned int volatile *reg, int val)
