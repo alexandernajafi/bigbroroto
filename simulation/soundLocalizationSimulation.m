5a3b617b961edd070b51d07a51e3d96a7754857b
Henrik Felding <henrik.felding@gmail.com>
2015-04-15 13:23:12 +0200

    Simulation of sound file

--- simulation/soundLocalizationSimulation.m
+++ simulation/soundLocalizationSimulation.m
@@ -4,22 +4,25 @@ close all
 clc
 
 sinc = @(x) sin(pi*x+1E-8)./(pi*x + 1E-8);
+[z,Fs] = wavread('sound');
 
-Fs = 8000;                      %Sample Frequency [Hz]
+sound(z,Fs);
+buffersize = 1024;
+y = buffer(z,buffersize);
+
+%Fs = 8000;                      %Sample Frequency [Hz]
 v = 330;                        %Speed of sound [m/s]
-phi = 5.14;                      %XY-plane angle [rad]
-theta = 0;                      %Z-axis angle [rad]
+phi = 2.54;                      %XY-plane angle [rad]
 d = 0.04;                       %Distance between microphones [m]
+numberOfPackages = 10;
 
 %       h1              h2
 %          Microphones
 %       h3              h4
 
-
-
 %Simulation of sound to microphone below
 
-                                %Time difference between microphones
+                        %Time difference between microphones
 dtA = cos(phi)*d/v;     %Top horizontal pair
 dtB = sin(phi)*d/v;     %Right vertical pair
 dtC = cos(phi)*d/v;     %Bottom horizontal pair
@@ -35,53 +38,75 @@ h2 = sinc((-10:10)-0);
 h3 = sinc((-10:10)-dphaseB-dphaseC);
 h4 = sinc((-10:10)-dphaseD);
 
-x = randn(1024,1);              %Random noise
+%x = randn(1024,1);              %Random noise
 
-x1 = filter(h1,1,x);            %Apply random noise to microphones
-x2 = filter(h2,1,x);
-x3 = filter(h3,1,x);
-x4 = filter(h4,1,x);
-
-%Simulation Finnish
-
-%Algorithm start
-
-X1 = fft(x1);                   %Fouriertransform of microphones
-X2 = fft(x2);
-X3 = fft(x3);
-X4 = fft(x4);
-                                %Cross spectrum calculation
-GA = X2.*conj(X1);
-GB = X2.*conj(X4);
-GC = X4.*conj(X3);
-GD = X1.*conj(X3);
-
-w = (0:1023)*2*pi/1024;
-
-tauA = 0;
-tauB = 0;
-tauC = 0;
-tauC = 0;
-maxValue = 0;
-maxAngle = 0;
-
-nbrofvalues = 1000;
-tempAngleVector = linspace(0,2*pi,nbrofvalues);
-
-for i = 1:nbrofvalues
-    tempAngle = tempAngleVector(i);
+%Begin for loop
+results = [];
+limit = floor(length(y(1,:)/numberOfPackages))-10;
+for i = 1:numberOfPackages:limit
     
-    pairA = sum(GA(10:502).*exp(-1*j*w(10:502)'*cos(tempAngle)*Fs*d/v)); %Top horizontal pair
-    pairB = sum(GB(10:502).*exp(-1*j*w(10:502)'*sin(tempAngle)*Fs*d/v)); %Right vertical pair
-    pairC = sum(GC(10:502).*exp(-1*j*w(10:502)'*cos(tempAngle)*Fs*d/v)); %Bottom horizontal pair
-    pairD = sum(GD(10:502).*exp(-1*j*w(10:502)'*sin(tempAngle)*Fs*d/v)); %Left vertical pair
+    GAmean = 0;
+    GBmean = 0;
+    GCmean = 0;
+    GDmean = 0;
     
-    value = pairA + pairB + pairC + pairD;
     
-    if (value >= maxValue),
-        maxValue = value;
-        maxAngle = tempAngle;
+    for n = 1:numberOfPackages
+        if (i == limit && n == numberOfPackages)
+            
+        end
+        x = y(:,i+n);
+        x1 = filter(h1,1,(x));%+randn([buffersize,1])/1000));            %Apply signal to microphones
+        x2 = filter(h2,1,(x));%+randn([buffersize,1])/1000)); 
+        x3 = filter(h3,1,(x));%+randn([buffersize,1])/1000)); 
+        x4 = filter(h4,1,(x));%+randn([buffersize,1])/1000)); 
+
+        X1 = fft(x1);                   %Fouriertransform of microphones
+        X2 = fft(x2);
+        X3 = fft(x3);
+        X4 = fft(x4);
+        %Cross spectrum calculation
+        GA = X2.*conj(X1);
+        GB = X2.*conj(X4);
+        GC = X4.*conj(X3);
+        GD = X1.*conj(X3);
+        
+        GAmean = (GAmean*(n-1)+GA)/n;
+        GBmean = (GBmean*(n-1)+GB)/n;
+        GCmean = (GCmean*(n-1)+GC)/n;
+        GDmean = (GDmean*(n-1)+GD)/n;
+        
     end
+        
+    w = (0:1023)*2*pi/buffersize;
+
+    %Medelvärdesbilda här. Bufferfunktionen.
+
+    maxValuePhi = 0;
+    maxAnglePhi = 0;
+
+    nbrofvalues = 1000;
+    tempAngleVector = linspace(0,2*pi,nbrofvalues);
+
+    for i = 1:nbrofvalues
+        tempAngle = tempAngleVector(i);
+        len = (buffersize/2-10);
+        pairA = sum(GAmean(10:len).*exp(-1*j*w(10:len)'*cos(tempAngle)*Fs*d/v)); %Top horizontal pair
+        pairB = sum(GBmean(10:len).*exp(-1*j*w(10:len)'*sin(tempAngle)*Fs*d/v)); %Right vertical pair
+        pairC = sum(GCmean(10:len).*exp(-1*j*w(10:len)'*cos(tempAngle)*Fs*d/v)); %Bottom horizontal pair
+        pairD = sum(GDmean(10:len).*exp(-1*j*w(10:len)'*sin(tempAngle)*Fs*d/v)); %Left vertical pair
+
+        value = pairA + pairB + pairC + pairD;
+
+        if (value >= maxValuePhi),
+            maxValuePhi = value;
+            maxAnglePhi = tempAngle;
+        end
+    end
+    
+    
+    results = [results maxAnglePhi];
 end
 
-maxAngle
+plot(results,'o')
+
