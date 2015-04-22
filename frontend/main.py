77c78492c7b1965aa60d3e9071cdf29948ac7f43
Alexander Najafi <alexandernajafi@Alexanders-MacBook-Pro.local>
2015-04-22 11:46:28 +0200

    Added basic web-server script for frontend

---
+++ frontend/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from flask import Flask
+import cherrypy
+from paste.translogger import TransLogger
+
+app = Flask(__name__)
+app.debug = True
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+def run_server():
+    app_logged = TransLogger(app)
+    cherrypy.tree.graft(app_logged, '/')
+    cherrypy.config.update({
+        'engine.autoreload_on': True,
+        'log.screen': True,
+        'server.socket_port': 5000,
+        'server.socket_host': 'localhost'
+    })
+    cherrypy.engine.start()
+    cherrypy.engine.block()
+
+if __name__ == "__main__":
+    run_server()
