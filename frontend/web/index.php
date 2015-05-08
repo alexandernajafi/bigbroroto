23f066c3e6a4e7f046aeb9da44c94782a55f95db
BigBroRoto <bigbroroto@steeze.com>
2015-05-08 13:54:48 +0000

    Added basic php script

---
+++ frontend/web/index.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title> BigBroRoto </title>
+		<link rel="stylesheet" href="static/bootstrap.min.css" />
+		<script src="static/jquery.min.js"></script>
+		<script src="static/bootstrap.min.js"></script>
+	</head>
+
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<br />
+					<br />
+					<h1> BigBroRoto </h1>
+					<br />
+					<br />
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-8">
+					<div class="panel panel-default">
+						<div class="panel-body">
+							<SCRIPT LANGUAGE="JavaScript">
+// Set the BaseURL to the URL of your camera
+var BaseURL = "http://192.168.10.101/";
+
+// DisplayWidth & DisplayHeight specifies the displayed width & height of the image.
+// You may change these numbers, the effect will be a stretched or a shrunk image
+var DisplayWidth = "720";
+var DisplayHeight = "580";
+
+// This is the path to the image generating file inside the camera itself
+var File = "axis-cgi/mjpg/video.cgi?resolution=CIF";
+// No changes required below this point
+var output = "";
+if ((navigator.appName == "Microsoft Internet Explorer") &&
+   (navigator.platform != "MacPPC") && (navigator.platform != "Mac68k"))
+{
+  // If Internet Explorer under Windows then use ActiveX 
+  output  = '<OBJECT ID="Player" width='
+  output += DisplayWidth;
+  output += ' height=';
+  output += DisplayHeight;
+  output += ' CLASSID="CLSID:DE625294-70E6-45ED-B895-CFFA13AEB044" ';
+  output += 'CODEBASE="';
+  output += BaseURL;
+  output += 'activex/AMC.cab#version=4,1,4,5">';
+  output += '<PARAM NAME="MediaURL" VALUE="';
+  output += BaseURL;
+  output += File + '">';
+  output += '<param name="MediaType" value="mjpeg-unicast">';
+  output += '<param name="ShowStatusBar" value="0">';
+  output += '<param name="ShowToolbar" value="0">';
+  output += '<param name="AutoStart" value="1">';
+  output += '<param name="StretchToFit" value="1">';
+  // Remove the // for the ptz settings below to use the code for click-in-image. 
+     //  output += '<param name="PTZControlURL" value="';
+     //  output += BaseURL;
+     //  output += '/axis-cgi/com/ptz.cgi?camera=1">';
+     //  output += '<param name="UIMode" value="ptz-relative">'; // or "ptz-absolute"
+  output += '<BR><B>Axis Media Control</B><BR>';
+  output += 'The AXIS Media Control, which enables you ';
+  output += 'to view live image streams in Microsoft Internet';
+  output += ' Explorer, could not be registered on your computer.';
+  output += '<BR></OBJECT>';
+} else {
+  // If not IE for Windows use the browser itself to display
+  theDate = new Date();
+  output  = '<IMG SRC="';
+  output += BaseURL;
+  output += File;
+  output += '&dummy=' + theDate.getTime().toString(10);
+  output += '" HEIGHT="';
+  output += DisplayHeight;
+  output += '" WIDTH="';
+  output += DisplayWidth;
+  output += '" ALT="Camera Image">';
+}
+document.write(output);
+document.Player.ToolbarConfiguration = "play,+snapshot,+fullscreen"
+
+// Remove the // below to use the code for Motion Detection. 
+  // document.Player.UIMode = "MDConfig";
+  // document.Player.MotionConfigURL = "/axis-cgi/operator/param.cgi?ImageSource=0"
+  // document.Player.MotionDataURL = "/axis-cgi/motion/motiondata.cgi";
+</SCRIPT>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="panel panel-default">
+						<div class="panel-body">
+							<div class="row">
+								<div class="col-sm-12">
+									<div class="alert alert-success"><center><b>Online!</b></center></div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-sm-6">
+									<button class="btn btn-success" style="width:100%">Start</button>
+								</div>
+								<div class="col-sm-6">
+									<button class="btn btn-danger" style="width:100%">Stop</button>
+								</div>
+							</div>
+							<div class="row" style="padding-top:20px">
+								<div class="col-sm-12">
+									<textarea style="width:100%;" rows="6">Logs...</textarea>
+								</div>
+							</div>
+							<div class="row" style="padding-top:20px">
+								<div class="col-sm-6">
+									<div class="panel panel-default">
+										<div class="panel-body">
+											<h2><center>65°</center></h2>
+										</div>
+									</div>
+								</div>
+								<div class="col-sm-6">
+									<div class="panel panel-default">
+										<div class="panel-body">
+											<h2><center>pil</center></h2>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-sm-12">
+									<button class="btn btn-primary" style="width:100%" data-toggle="modal" data-target="#settingsModal">
+										Settings
+									</button>
+
+									<div class="modal fade" id="settingsModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+										<div class="modal-dialog">
+	    									<div class="modal-content">
+	    										<div class="modal-header">
+	    											<h3>Settings</h3>
+	    										</div>
+	    										<div class="modal-body">
+	    											<form>
+	    												<h3>Camera</h3>
+		    											<div class="form-group">
+		    												<label>Camera IP</label>
+		    												<input type="text" class="form-control" placeholder="Enter IP" />
+		    											</div>
+		    											<div class="form-group">
+		    												<label>Camera IP</label>
+		    												<input type="text" class="form-control" placeholder="Enter IP" />
+		    											</div>
+	    											</form>
+	    										</div>
+	    									</div>
+	    								</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+	</body>
+</html>
