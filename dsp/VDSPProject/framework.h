30074672ae12908a642f7de1ecb71770d0f3b75b
Linus Hellman <linkan360@gmail.com>
2015-05-11 10:51:19 +0200

    Algorithm trash aint workin

--- dsp/VDSPProject/framework.h
+++ dsp/VDSPProject/framework.h
@@ -2,10 +2,13 @@
 #define framework_h_included
 
 /* Configuration */
-#define DSP_SAMPLE_RATE         16000
-#define DSP_BLOCK_SIZE          32
-#define DSP_INPUT_GAIN          10
-#define DSP_OUTPUT_ATTENUATION  0
+#define DSP_SAMPLE_RATE         8000
+#define DSP_BLOCK_SIZE          256
+#define DSP_INPUT_GAIN          20
+#define DSP_OUTPUT_ATTENUATION  10
+
+/* Codec selection */
+//#define DSP_USE_DOUBLE_RATE
 
 /* Constants */
 #define DSP_FREQUENCY           (12288000*16/2)
@@ -19,18 +22,12 @@ typedef struct {
 void dsp_init(void);
 void dsp_start(void);
 void dsp_stop(void);
-sample_t *dsp_get_audio(void);
 unsigned int dsp_get_keys(void);
 unsigned int dsp_get_cycles(void);
 void dsp_set_leds(unsigned int value);
-void spi_init_slave(void);
-//void spi_send(void);
-void spi_read(void);
-void wait(int time);
-void test(void);
-void send_wait(void);
-void algorithm(void);
-void algorithm_setup(void);
-void algorithm_close(void);
-void spi_send(int);
+
+sample_t *dsp_get_audio(void);
+sample_t *dsp_get_audio_01(void);
+sample_t *dsp_get_audio_23(void);
+
 #endif
